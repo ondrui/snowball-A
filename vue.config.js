922b209ef3a6c67634ceba7d2bd4dd9455d9856b
ondrui <ondrui@yandex.ru>
2023-05-12 12:00:29 +0300

    adding dist subtree

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,7 @@ module.exports = defineConfig({
 });
 
 module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "./" : "/",
   chainWebpack: (config) => {
     config.module.rules.delete("svg");
   },
@@ -47,5 +48,4 @@ module.exports = {
     host: "localhost",
     // allowedHosts: "all",
   },
-  publicPath: process.env.NODE_ENV === "production" ? "/snowball-A/" : "/",
 };
