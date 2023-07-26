d3ef1f3eeb51e46d6a0edde5e1af1b51ffed70a5
ondrui <ondrui@yandex.ru>
2023-07-26 10:41:32 +0300

    -NEW- add and use TheSpinner component

--- src/components/UI/index.js
+++ src/components/UI/index.js
@@ -1,6 +1,7 @@
 import BaseIcon from "./BaseIcon.vue";
 import SearchBar from "./SearchBar.vue";
+import TheSpinner from "./TheSpinner.vue";
 import BreadCrumbs from "./BreadCrumbs.vue";
 import SwitcherRadioBtn from "./SwitcherRadioBtn.vue";
 
-export default [BaseIcon, SwitcherRadioBtn, SearchBar, BreadCrumbs];
+export default [BaseIcon, SwitcherRadioBtn, SearchBar, BreadCrumbs, TheSpinner];
